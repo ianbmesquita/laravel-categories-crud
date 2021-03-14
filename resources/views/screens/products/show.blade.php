ac4432bb2be301e522136dffd3ad3e8f1e0e0a63
Ian Mesquita <ianmesquitadb@gmail.com>
2021-03-14 20:17:32 -0300

    Listing the registered products.

---
+++ resources/views/screens/products/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app', ["current" => "products"])
+
+@section('body')
+    <div class="card border">
+        <div class="card-body">
+            <h5 class="card-title"><strong>Produtos cadastrados</strong></h5>
+
+                <table class="table table-ordered table-hover">
+                    <thead>
+                        <tr>
+                            <td>Código</td>
+                            <td>Nome</td>
+                            <td>Estoque</td>
+                            <td>Preço</td>
+                            <td>Ações</td>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($products as $product)
+                            <tr>
+                                <td>{{ $product->id }}</td>
+                                <td>{{ $product->name }}</td>
+                                <td>{{ $product->stock }}</td>
+                                <td>{{ $product->price }}</td>
+                                <td>
+                                    <a href="/products/{{ $product->id }}/edit" class="btn btn-primary btn-sm">Editar</a>
+                                    <a href="/products/{{ $product->id }}/destroy" class="btn btn-danger btn-sm">Excluir</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+        </div>
+
+        <div class="card-footer">
+            <a href="/products/new" class="btn btn-primary btn-sm">Novo produto</a>
+        </div>
+    </div>
+@endsection
